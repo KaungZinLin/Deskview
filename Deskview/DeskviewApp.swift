ed8c0bbd07d1686e6193bca471b34d0f74f77497
KaungZinLin <kaung.zl2011@gmail.com>
2024-11-07 19:27:44 +0630

    Initial Commit

---
+++ Deskview/DeskviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskviewApp.swift
+//  Deskview
+//
+//  Created by Kaung Zin Lin on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DeskviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeskviewTests/DeskviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeskviewTests.swift
+//  DeskviewTests
+//
+//  Created by Kaung Zin Lin on 11/7/24.
+//
+
+import Testing
+@testable import Deskview
+
+struct DeskviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
